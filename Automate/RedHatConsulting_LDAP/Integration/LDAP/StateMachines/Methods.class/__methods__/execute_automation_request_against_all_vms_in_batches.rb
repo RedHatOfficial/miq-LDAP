b3e451f3b678a82a5f01bae3c36680961cda7270
Ian Tewksbury <itewk@redhat.com>
2018-02-27 02:41:11 -0500

    Add ability to edit LDAP entries on a per VM bases or by doing a service reconfigure

--- Automate/RedHatConsulting_LDAP/Integration/LDAP/StateMachines/Methods.class/__methods__/execute_automation_request_against_all_vms_in_batches.rb
+++ Automate/RedHatConsulting_LDAP/Integration/LDAP/StateMachines/Methods.class/__methods__/execute_automation_request_against_all_vms_in_batches.rb
@@ -1,10 +1,15 @@
-# For every VM in a given container of VMs update the VM's Tags and Custom Attributes based on the LDAP record for that VM.
+# Collect all the VMs in a given container of VMs and execute a given automation request against batches of those VMs.
 #
 # EXPECTED
+#   EVM INPUTS
+#     automation_request_instance_name         - Automation instance name to invoke for each batch of VMs.
+#
 #   EVM ROOT
 #     ext_management_system, ems_cluster, host - One of these is expected to be set as the parent of the group of VMs to update.
 #
 @DEBUG = false
+
+require 'json'
 
 DEFAULT_VMS_BATCH_SIZE = 10
 LDAP_CONFIG_URI        = 'Integration/LDAP/Configuration/default'
@@ -17,12 +22,6 @@ def error(msg)
   $evm.root['ae_result'] = 'error'
   $evm.root['ae_reason'] = msg.to_s
   exit MIQ_STOP
-end
-
-def dump_current
-  $evm.log("info", "Listing Current Object Attributes:") 
-  $evm.current.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}") }
-  $evm.log("info", "===========================================") 
 end
 
 def dump_root
@@ -42,18 +41,21 @@ def get_ldap_config()
   return $evm.instantiate(LDAP_CONFIG_URI)
 end
 
-# Updates the Tags and Custom Attributes from LDAP for a batch of VMs.
+# Execute a given automation request for a given batch of VMs.
 #
-# @param vm_ids IDs of the VMs to update the Tags and Custom Attributes for from LDAP.
-def update_tags_and_custom_attributes_from_ldap_entries_for_batch_of_vms(vm_ids)
+# @param vm_ids                           IDs of the VMs to execute the automation request for.
+# @param automation_request_instance_name Instance name to execute.
+# @param additional_attrs                 Additional attributes to pass when executing the automation request.
+def execute_automation_request_for_batch_of_vms(vm_ids, automation_request_instance_name, additional_attrs)
   attrs = {
     'vm_ids' => vm_ids.join(',')
   }
+  attrs = additional_attrs.merge(attrs) if additional_attrs
 
   options = {}
   options[:namespace]     = 'Integration/LDAP/StateMachines'
   options[:class_name]    = 'Methods'
-  options[:instance_name] = 'update_tags_and_custom_attributes_from_ldap_entries_for_batch_of_vms'
+  options[:instance_name] = automation_request_instance_name
   options[:user_id]       = $evm.root['user'].id
   options[:attrs]         = attrs
   approving_user = 'admin'
@@ -64,26 +66,49 @@ def update_tags_and_custom_attributes_from_ldap_entries_for_batch_of_vms(vm_ids)
 end
 
 begin
-  # determine the VMs to update
+  dump_root() if @DEBUG
+  
+  # determine the VMs to execute the automation request against
   vmdb_object_type = $evm.root['vmdb_object_type']
   if (vmdb_object_type == 'ext_management_system') || (vmdb_object_type == 'automation_task' && !$evm.root['ext_management_system'].nil?)
     ems = $evm.root['ext_management_system']
-    $evm.log(:info, "ems=#{ems.name}") if @DEBUG
+    $evm.log(:info, "ems => #{ems.name}") if @DEBUG
     
     vms = ems.vms
   elsif (vmdb_object_type == 'ems_cluster') || (vmdb_object_type == 'automation_task' && !$evm.root['ems_cluster'].nil?)
     cluster = $evm.root['ems_cluster']
-    $evm.log(:info, "cluster=#{cluster.name}") if @DEBUG
+    $evm.log(:info, "cluster => #{cluster.name}") if @DEBUG
     
     vms = cluster.vms
   elsif (vmdb_object_type == 'host') || (vmdb_object_type == 'automation_task' && !$evm.root['host'].nil?)
     host = $evm.root['host']
-      $evm.log(:info, "host=#{host.name}") if @DEBUG
+    $evm.log(:info, "host => #{host.name}") if @DEBUG
     
-      vms = host.vms
+    vms = host.vms
+  elsif (vmdb_object_type == 'service_reconfigure_task')
+    service_reconfigure_task = $evm.root['service_reconfigure_task']
+    dump_object("service_reconfigure_task", service_reconfigure_task) if @DEBUG
+    
+    # get the service
+    service_id = service_reconfigure_task.source_id
+    service = $evm.vmdb(:service).find_by_id(service_id)
+    
+    # get the additional otpions
+    additional_attrs = {
+      :options => service_reconfigure_task.options.to_json # hash wont survive all the passing through executes so turn it to json to be transformed later
+    }
+    
+    # get the VMs
+    vms = service.vms
   else
-    error("$evm.root['vmdb_object_type']=#{$evm.root['vmdb_object_type']} is not one of expected ['ext_management_system', 'ems_cluster', 'host','automation_task'].")
+    error("$evm.root['vmdb_object_type']='#{$evm.root['vmdb_object_type']}' is not one of expected " +
+          "['ext_management_system', 'ems_cluster', 'host', 'service_reconfigure_task', 'automation_task'].")
   end
+  
+  # get the automationr request to invoke in batches
+  automation_request_instance_name = $evm.inputs['automation_request_instance_name']
+  error("automation_request_instance_name msut be specified") if automation_request_instance_name.blank?
+  $evm.log(:info, "automation_request_instance_name = #{automation_request_instance_name}") if @DEBUG
   
   # determine the size of each batch of VMs to process
   ldap_config = get_ldap_config()
@@ -94,6 +119,6 @@ begin
   $evm.log(:info, "VMs to Update in Batches: { vms => #{vms.collect { |vm| vm.name }} }") if @DEBUG
   vms.each_slice(vms_batch_size) do |vms_batch|
     vm_ids = vms_batch.collect { |vm| vm.id }
-    update_tags_and_custom_attributes_from_ldap_entries_for_batch_of_vms(vm_ids)
+    execute_automation_request_for_batch_of_vms(vm_ids, automation_request_instance_name, additional_attrs)
   end
 end
