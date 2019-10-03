176a3aedad2013445044374a739d19d15540dfbe
Andrew Becker <A-Beck@users.noreply.github.com>
2019-10-03 12:23:00 -0400

    Support CFME 5.10 retirement tasks (#51)
    + introduce dependency on miq-utilities
    + limited refactor to class sytle

--- Automate/RedHatConsulting_LDAP/Integration/LDAP/Operations/Methods.class/__methods__/get_ldap_new_entry_attributes.rb
+++ Automate/RedHatConsulting_LDAP/Integration/LDAP/Operations/Methods.class/__methods__/get_ldap_new_entry_attributes.rb
@@ -10,177 +10,148 @@
 #   EVM OBJECT
 #     'ldap_new_entry_attributes' - Array of LDAP attributes to set on the new LDAP entry for the given VM.
 #     'ldap_new_entry_dn'         - The DN of the new LDAP entry to create for the given VM.
-@DEBUG = false
 
-# IMPLEMENTERS: Update with business logic
-#
-# Given a VM hostname returns the base attributes required to add a new LDAP entry for that host.
-#
-# @param vm_hostname       Hostname of the VM to get the new LDAP entry attributes for
-# @param vm                VM to get the new LDAP entry attributes for
-# @param dialog_attributes Dialog attributes which can be used to set the base attributes,
-#                          required to create the new LDAP entry
-# @param ldap_config       LDAP configuration information
-#
-# @return Hash of LDAP attributes to use to create a new LDAP entry for the given VM hostname
-def get_ldap_new_entry_attributes(vm_hostname, vm, dialog_attributes, ldap_config)
-  
-  # IMPLEMENTERS: Change as necessary.
-  #               This reference works with Red Hat IdM / FreeIPA.
-  #
-  krb_principal_domain_name = ldap_config['krb_principal_domain_name']
-  $evm.log(:info, "krb_principal_domain_name=#{krb_principal_domain_name}") if @DEBUG
-  return {
-    :cn               => vm_hostname,
-    :fqdn             => vm_hostname,
-    :objectclass      => ['ipaobject', 'ieee802device', 'nshost', 'ipaservice', 'pkiuser', 'ipahost', 'krbprincipal', 'krbprincipalaux', 'ipasshhost', 'top', 'ipaSshGroupOfPubKeys'],
-    :krbPrincipalName => "host/#{vm_hostname}@#{krb_principal_domain_name}",
-    :ipaUniqueID      => 'autogenerate'
-  }
-end
+# / Integration / LDAP / Operations / Methods / get_ldap_new_entry_attributes
+module RedHatConsulting_LDAP
+  module Integration
+    module LDAP
+      module Operations
+        module Methods
+          class GetLDAPNewEntryAttributes
 
-# IMPLEMENTERS: Update with business logic
-#
-# Returns the DN for a new LDAP entry for the given VM hostname.
-#
-# @param vm_hostname       Hostname of VM to create the DN for the new LDAP entry
-# @param vm                VM to create the DN for the new LDAP entry
-# @param dialog_attributes Dialog attributes which can be used to create the DN for the new LDAP entry
-# @param ldap_config       LDAP configuration information
-#
-# @return Full DN for a new LDAP entry for the given VM hostname
-def get_ldap_new_entry_dn(vm_hostname, vm, dialog_attributes, ldap_config)
-  
-  # IMPLEMENTERS: Change as necessary.
-  #               This reference works with Red Hat IdM / FreeIPA.
-  #
-  ldap_treebase = ldap_config['ldap_treebase']
-  ldap_hostname_filter = ldap_config['ldap_hostname_filter']
-  $evm.log(:info, "ldap_treebase=#{ldap_treebase}")               if @DEBUG
-  $evm.log(:info, "ldap_hostname_filter=#{ldap_hostname_filter}") if @DEBUG
-  
-  ldap_new_entry_dn = "#{ldap_hostname_filter}=#{vm_hostname},#{ldap_treebase}"
-  
-  return ldap_new_entry_dn
-end
+            include RedHatConsulting_Utilities::StdLib::Core
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# Log an error and exit.
-#
-# @param msg Message to error with
-def error(msg)
-  $evm.log(:error, msg)
-  $evm.root['ae_result'] = 'error'
-  $evm.root['ae_reason'] = msg.to_s
-  exit MIQ_STOP
-end
+            # IMPLEMENTERS: DO NOT MODIFY
+            def initialize(handle = $evm)
+              @handle = handle
+              @DEBUG = false
+            end
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# There are many ways to attempt to pass parameters in Automate.
-# This function checks all of them in priorty order as well as checking for symbol or string.
-#
-# Order:
-#   1. Current
-#   2. Object
-#   3. Root
-#   4. State
-#
-# @return Value for the given parameter or nil if none is found
-def get_param(param)  
-  # else check if current has been set for given param
-  param_value ||= $evm.current[param.to_sym]
-  param_value ||= $evm.current[param.to_s]
- 
-  # else cehck if current has been set for given param
-  param_value ||= $evm.object[param.to_sym]
-  param_value ||= $evm.object[param.to_s]
-  
-  # else check if param on root has been set for given param
-  param_value ||= $evm.root[param.to_sym]
-  param_value ||= $evm.root[param.to_s]
-  
-  # check if state has been set for given param
-  param_value ||= $evm.get_state_var(param.to_sym)
-  param_value ||= $evm.get_state_var(param.to_s)
+            # IMPLEMENTERS: Update with business logic
+            #
+            # Given a VM hostname returns the base attributes required to add a new LDAP entry for that host.
+            #
+            # @param vm_hostname       Hostname of the VM to get the new LDAP entry attributes for
+            # @param vm                VM to get the new LDAP entry attributes for
+            # @param dialog_attributes Dialog attributes which can be used to set the base attributes,
+            #                          required to create the new LDAP entry
+            # @param ldap_config       LDAP configuration information
+            #
+            # @return Hash of LDAP attributes to use to create a new LDAP entry for the given VM hostname
+            def get_ldap_new_entry_attributes(vm_hostname, vm, dialog_attributes, ldap_config)
 
-  $evm.log(:info, "{ '#{param}' => '#{param_value}' }") if @DEBUG
-  return param_value
-end
+              # IMPLEMENTERS: Change as necessary.
+              #               This reference works with Red Hat IdM / FreeIPA.
+              #
+              krb_principal_domain_name = ldap_config['krb_principal_domain_name']
+              log(:info, "krb_principal_domain_name=#{krb_principal_domain_name}") if @DEBUG
+              return {
+                :cn               => vm_hostname,
+                :fqdn             => vm_hostname,
+                :objectclass      => ['ipaobject', 'ieee802device', 'nshost', 'ipaservice', 'pkiuser', 'ipahost', 'krbprincipal', 'krbprincipalaux', 'ipasshhost', 'top', 'ipaSshGroupOfPubKeys'],
+                :krbPrincipalName => "host/#{vm_hostname}@#{krb_principal_domain_name}",
+                :ipaUniqueID      => 'autogenerate'
+                }
+            end
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# Gets the LDAP connection configuration information.
-LDAP_CONFIG_URI = 'Integration/LDAP/Configuration/default'
-def get_ldap_config()
-  return $evm.instantiate(LDAP_CONFIG_URI)
-end
+            # IMPLEMENTERS: Update with business logic
+            #
+            # Returns the DN for a new LDAP entry for the given VM hostname.
+            #
+            # @param vm_hostname       Hostname of VM to create the DN for the new LDAP entry
+            # @param vm                VM to create the DN for the new LDAP entry
+            # @param dialog_attributes Dialog attributes which can be used to create the DN for the new LDAP entry
+            # @param ldap_config       LDAP configuration information
+            #
+            # @return Full DN for a new LDAP entry for the given VM hostname
+            def get_ldap_new_entry_dn(vm_hostname, vm, dialog_attributes, ldap_config)
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# Get the hostname for a given VM.
-#
-# @param vm VM to get the hostname for
-#
-# @return Hostname of the VM or vm.name if a hostname can not be found
-def get_vm_hostname(vm)
-  hostname = vm.hardware.hostnames.first rescue nil
-  hostname = vm.name if hostname.blank?
-  return hostname
-end
+              # IMPLEMENTERS: Change as necessary.
+              #               This reference works with Red Hat IdM / FreeIPA.
+              #
+              ldap_treebase = ldap_config['ldap_treebase']
+              ldap_hostname_filter = ldap_config['ldap_hostname_filter']
+              log(:info, "ldap_treebase=#{ldap_treebase}")               if @DEBUG
+              log(:info, "ldap_hostname_filter=#{ldap_hostname_filter}") if @DEBUG
 
-# IMPLEMENTERS: DO NOT MODIFY
-begin
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # get the parameters
+              ldap_new_entry_dn = "#{ldap_hostname_filter}=#{vm_hostname},#{ldap_treebase}"
 
-  # get the VM to get the new LDAP entry attributes for and
-  # get the service dialog attributes associated with that VM
-  $evm.log(:info, "$evm.root['vmdb_object_type'] => '#{$evm.root['vmdb_object_type']}'.") if @DEBUG
-  case $evm.root['vmdb_object_type']
-    when 'miq_provision'
-      miq_provision     = $evm.root['miq_provision']
-      vm                = miq_provision.vm
-      dialog_attributes = miq_provision.options
-    when 'vm'
-      vm = $evm.root['vm']
-      dialog_attributes = $evm.root.attributes
-    else
-      error("Can not handle vmdb_object_type: #{$evm.root['vmdb_object_type']}")
+              return ldap_new_entry_dn
+            end
+
+            def main
+              begin
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # get the parameters
+
+                # get the VM to get the new LDAP entry attributes for and
+                # get the service dialog attributes associated with that VM
+                vm, dialog_attributes = get_vm_and_options()
+                error("vm parameter not found")                              if vm.blank?
+                log(:info, "vm => #{vm.name}")                          if @DEBUG
+                log(:info, "dialog_attributes => #{dialog_attributes}") if @DEBUG
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # determine LDAP treebase and hostname filter
+                ldap_config = get_ldap_config()
+                error("LDAP Configuration not found") if ldap_config.nil?
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # determine the VM hostname
+                vm_hostname = get_vm_hostname(vm)
+                log(:info, "vm_hostname => '#{vm_hostname}'") if @DEBUG
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # get information about the new LDAP entry
+                ldap_new_entry_attributes = get_ldap_new_entry_attributes(vm_hostname, vm, dialog_attributes, ldap_config)
+                ldap_new_entry_dn         = get_ldap_new_entry_dn(vm_hostname, vm, dialog_attributes, ldap_config)
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # return new LDAP entry attributes
+                @handle.object['ldap_new_entry_attributes'] = ldap_new_entry_attributes
+                log(:info, "$evm.object['ldap_new_entry_attributes']=#{@handle.object['ldap_new_entry_attributes']})") if @DEBUG
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # return new LDAP entry DN
+                @handle.object['ldap_new_entry_dn'] = ldap_new_entry_dn
+                log(:info, "$evm.object['ldap_new_entry_dn']=#{@handle.object['ldap_new_entry_dn']})") if @DEBUG
+              end      
+            end
+
+            # IMPLEMENTERS: DO NOT MODIFY
+            #
+            # Get the hostname for a given VM.
+            #
+            # @param vm VM to get the hostname for
+            #
+            # @return Hostname of the VM or vm.name if a hostname can not be found
+            def get_vm_hostname(vm)
+              hostname = vm.hardware.hostnames.first rescue nil
+              hostname = vm.name if hostname.blank?
+              return hostname
+            end
+
+            # IMPLEMENTERS: DO NOT MODIFY
+            #
+            # Gets the LDAP connection configuration information.
+            LDAP_CONFIG_URI = 'Integration/LDAP/Configuration/default'
+            def get_ldap_config()
+              return @handle.instantiate(LDAP_CONFIG_URI)
+            end
+
+          end
+        end
+      end
+    end
   end
-  error("vm parameter not found")                              if vm.blank?
-  $evm.log(:info, "vm => #{vm.name}")                          if @DEBUG
-  $evm.log(:info, "dialog_attributes => #{dialog_attributes}") if @DEBUG
-  
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # determine LDAP treebase and hostname filter
-  ldap_config = get_ldap_config()
-  error("LDAP Configuration not found") if ldap_config.nil?
-  
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # determine the VM hostname
-  vm_hostname = get_vm_hostname(vm)
-  $evm.log(:info, "vm_hostname => '#{vm_hostname}'") if @DEBUG
-  
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # get information about the new LDAP entry
-  ldap_new_entry_attributes = get_ldap_new_entry_attributes(vm_hostname, vm, dialog_attributes, ldap_config)
-  ldap_new_entry_dn         = get_ldap_new_entry_dn(vm_hostname, vm, dialog_attributes, ldap_config)
-  
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # return new LDAP entry attributes
-  $evm.object['ldap_new_entry_attributes'] = ldap_new_entry_attributes
-  $evm.log(:info, "$evm.object['ldap_new_entry_attributes']=#{$evm.object['ldap_new_entry_attributes']})") if @DEBUG
-  
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # return new LDAP entry DN
-  $evm.object['ldap_new_entry_dn'] = ldap_new_entry_dn
-  $evm.log(:info, "$evm.object['ldap_new_entry_dn']=#{$evm.object['ldap_new_entry_dn']})") if @DEBUG
+end
+
+if __FILE__ == $PROGRAM_NAME
+  RedHatConsulting_LDAP::Integration::LDAP::Operations::Methods::GetLDAPNewEntryAttributes.new.main
 end
--- Automate/RedHatConsulting_LDAP/Integration/LDAP/Operations/Methods.class/__methods__/munge_ldap_entry_attributes.rb
+++ Automate/RedHatConsulting_LDAP/Integration/LDAP/Operations/Methods.class/__methods__/munge_ldap_entry_attributes.rb
@@ -15,169 +15,105 @@
 #   EVM OBJECT
 #     'ldap_entry_attributes' - Hash of LDAP attributes to set on the LDAP entry for the given VM.
 #
-@DEBUG = false
 
 require 'rubygems'
 require 'net/ldap'
 require 'json'
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# Log an error and exit.
-#
-# @param msg Message to error with
-def error(msg)
-  $evm.log(:error, msg)
-  $evm.root['ae_result'] = 'error'
-  $evm.root['ae_reason'] = msg.to_s
-  exit MIQ_STOP
-end
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-def dump_root
-  $evm.log("info", "Listing Root Object Attributes:") 
-  $evm.root.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}") }
-  $evm.log("info", "===========================================") 
-end
+# / Integration / LDAP / Operations / Methods / munge_ldap_entry_attributes
+module RedHatConsulting_LDAP
+  module Integration
+    module LDAP
+      module Operations
+        module Methods
+          class MungeLDAPEntryAttributes
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-def dump_object(object_string, object)
-  $evm.log("info", "Listing #{object_string} Attributes:") 
-  object.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}") }
-  $evm.log("info", "===========================================") 
-end
+            include RedHatConsulting_Utilities::StdLib::Core
 
-# IMPLEMENTERS: DO NOT MODIFY
-#
-# There are many ways to attempt to pass parameters in Automate.
-# This function checks all of them in priorty order as well as checking for symbol or string.
-#
-# Order:
-#   1. Current
-#   2. Object
-#   3. Root
-#   4. State
-#
-# @return Value for the given parameter or nil if none is found
-def get_param(param)  
-  # else check if current has been set for given param
-  param_value ||= $evm.current[param.to_sym]
-  param_value ||= $evm.current[param.to_s]
- 
-  # else cehck if current has been set for given param
-  param_value ||= $evm.object[param.to_sym]
-  param_value ||= $evm.object[param.to_s]
-  
-  # else check if param on root has been set for given param
-  param_value ||= $evm.root[param.to_sym]
-  param_value ||= $evm.root[param.to_s]
-  
-  # check if state has been set for given param
-  param_value ||= $evm.get_state_var(param.to_sym)
-  param_value ||= $evm.get_state_var(param.to_s)
+            # IMPLEMENTERS: DO NOT MODIFY
+            #
+            # return munged LDAP entry attributes
+            def initialize(handle = $evm)
+              @handle = handle
+              @DEBUG  = false
+            end
 
-  $evm.log(:info, "{ '#{param}' => '#{param_value}' }") if @DEBUG
-  return param_value
-end
+            def main
+              begin
+                dump_root() if @DEBUG
 
-begin
-  dump_root() if @DEBUG
+                ldap_entry_attributes = {}
 
-  ldap_entry_attributes = {}
+                #If retiring else if provisioning or updating
+                if @handle.root['vmdb_object_type'] == 'vm_retire_task' or ( @handle.root['vmdb_object_type'] == 'vm' and @handle.root['event_type'] == 'request_vm_retire' )
+                  # IMPLIMENTORS: Modify as necessary
+                  # Use this section to define any LDAP modifications that should occur on retirement
+                  #
+                  #ldap_entry_attributes['sample_key'] = 'sample_value'
+                else
+                  # Depending on the vmdb_object_type get the required information from different sources
+                  #   * get the VM to get the LDAP entry attributes for and
+                  #   * get the service dialog attributes associated with that VM
+                  vm, dialog_attributes = get_vm_and_options
+                  error("vm parameter not found")      if vm.blank?
+                  error("dialog attributes not found") if dialog_attributes.blank?
 
-  #If retiring else if provisioning or updating
-  if $evm.root['vmdb_object_type'] == 'vm' and $evm.root['event_type'] == 'request_vm_retire'
-    # IMPLIMENTORS: Modify as necessary
-    # Use this section to define any LDAP modifications that should occur on retirement
-    #
-    #ldap_entry_attributes['sample_key'] = 'sample_value'
-  else
-    # Depending on the vmdb_object_type get the required information from different sources
-    #   * get the VM to get the LDAP entry attributes for and
-    #   * get the service dialog attributes associated with that VM
-    $evm.log(:info, "$evm.root['vmdb_object_type'] => '#{$evm.root['vmdb_object_type']}'.")
-    case $evm.root['vmdb_object_type']
-      when 'miq_provision'
-        $evm.log(:info, "Get VM and dialog attributes from $evm.root['miq_provision']") if @DEBUG
-        miq_provision     = $evm.root['miq_provision']
-        vm                = miq_provision.vm
-        dialog_attributes = miq_provision.options
- 
-        # IMPLIMENTORS: Modify as necessary
-        #               Get additional parameters
-      when 'vm'
-        $evm.log(:info, "Get VM from paramater and dialog attributes form $evm.root") if @DEBUG
-        vm                = get_param(:vm)
-        dialog_attributes = $evm.root.attributes
- 
-        # IMPLIMENTORS: Modify as necessary
-        #               Get additional parameters
-      when 'automation_task'
-        $evm.log(:info, "Get VM parameter and dialog attributes from $evm.root") if @DEBUG
-        automation_task = $evm.root['automation_task']
-        dump_object("automation_task", automation_task) if @DEBUG
-      
-        vm      = get_param(:vm)
-      
-        # get dialog attributes
-        options           = get_param(:options)
-        options           = JSON.load(options)     if options && options.class == String
-        options           = options.symbolize_keys if options
-        dialog_attributes = options[:dialog]       if options
-      
-        # IMPLIMENTORS: Modify as necessary
-        #               Get additional parameters
-      else
-        error("Can not handle vmdb_object_type: #{$evm.root['vmdb_object_type']}")
-    end
-    error("vm parameter not found")      if vm.blank?
-    error("dialog attributes not found") if dialog_attributes.blank?
-    
-    dialog_attributes = dialog_attributes.symbolize_keys
-    $evm.log(:info, "vm => #{vm.name}")                          if @DEBUG
-    $evm.log(:info, "dialog_attributes => #{dialog_attributes}") if @DEBUG
-    
-    # IMPLIMENTORS: Modify as necessary
-    #               Additional parameters verification
- 
-    # get the existing LDAP entries for the given VM
-    ldap_entries = get_param(:ldap_entries)
-    error("ldap_entries parameter not found") if ldap_entries.blank?
- 
-    # narrow down to a single LDAP entry in the off chance there is more then one
-    error("More then one existing LDAP_entry for VM (#{vm.name}) was found, unsure how to handle this case.") if ldap_entries.length > 1
-    ldap_entry = ldap_entries[0]
-    $evm.log(:info, "ldap_entry => #{ldap_entry}") if @DEBUG
-    
-    # IMPLEMENTERS: Modify as necessary
-    #               This implementation returns the existing LDAP entry attributes without modification
-    #
-    ldap_entry_attributes = {}
-    ldap_entry.each do |ldap_attribute, ldap_attribute_values|
-      ldap_entry_attributes[ldap_attribute.to_sym] = ldap_attribute_values
-      $evm.log(:info, "Set LDAP entry attribute to existing value: { ldap_attribute => #{ldap_attribute}, ldap_attribute_values => #{ldap_attribute_values} }") if @DEBUG
-    end
-    $evm.log(:info, "after merging in existing LDAP entry attributes: ldap_entry_attributes => #{ldap_entry_attributes}") if @DEBUG
-    
-    # IMPLEMENTERS: Modify as necessary
-    #               This implementation overrides any existing LDAP entry attributes with values from dialog elements with names that
-    #               start with 'dialog_ldap_entry_attribute_'.
-    #               All values are split on "\n" in case it is ment to be an array of values.
-    #
-    # for each LDAP entry attribute dialog element
-    dialog_attributes.select { |k, v| k.to_s.start_with?('dialog_ldap_entry_attribute_') }.each do |dialog_ldap_entry_attribute, dialog_ldap_entry_attribute_value|
-      $evm.log(:info, "LDAP entry attribute dialog element: { dialog_ldap_entry_attribute => #{dialog_ldap_entry_attribute}, dialog_ldap_entry_attribute_value => #{dialog_ldap_entry_attribute_value} }") if @DEBUG
-      dialog_ldap_entry_attribute_name = dialog_ldap_entry_attribute.to_s.match(/dialog_ldap_entry_attribute_(.*)/i).captures[0]
-      ldap_entry_attributes[dialog_ldap_entry_attribute_name] = dialog_ldap_entry_attribute_value.split("\n")
-      $evm.log(:info, "Set LDAP entry attribute to value from dialog: { ldap_attribute => #{dialog_ldap_entry_attribute_name}, ldap_attribute_values => #{ldap_entry_attributes[dialog_ldap_entry_attribute_name]} }") if @DEBUG
+                  dialog_attributes = dialog_attributes.symbolize_keys
+                  log(:info, "vm => #{vm.name}")                          if @DEBUG
+                  log(:info, "dialog_attributes => #{dialog_attributes}") if @DEBUG
+
+                  # IMPLIMENTORS: Modify as necessary
+                  #               Additional parameters verification
+
+                  # get the existing LDAP entries for the given VM
+                  ldap_entries = get_param(:ldap_entries)
+                  error("ldap_entries parameter not found") if ldap_entries.blank?
+
+                  # narrow down to a single LDAP entry in the off chance there is more then one
+                  error("More then one existing LDAP_entry for VM (#{vm.name}) was found, unsure how to handle this case.") if ldap_entries.length > 1
+                  ldap_entry = ldap_entries[0]
+                  log(:info, "ldap_entry => #{ldap_entry}") if @DEBUG
+
+                  # IMPLEMENTERS: Modify as necessary
+                  #               This implementation returns the existing LDAP entry attributes without modification
+                  #
+                  ldap_entry_attributes = {}
+                  ldap_entry.each do |ldap_attribute, ldap_attribute_values|
+                    ldap_entry_attributes[ldap_attribute.to_sym] = ldap_attribute_values
+                    log(:info, "Set LDAP entry attribute to existing value: { ldap_attribute => #{ldap_attribute}, ldap_attribute_values => #{ldap_attribute_values} }") if @DEBUG
+                  end
+                  log(:info, "after merging in existing LDAP entry attributes: ldap_entry_attributes => #{ldap_entry_attributes}") if @DEBUG
+
+                  # IMPLEMENTERS: Modify as necessary
+                  #               This implementation overrides any existing LDAP entry attributes with values from dialog elements with names that
+                  #               start with 'dialog_ldap_entry_attribute_'.
+                  #               All values are split on "\n" in case it is ment to be an array of values.
+                  #
+                  # for each LDAP entry attribute dialog element
+                  dialog_attributes.select { |k, v| k.to_s.start_with?('dialog_ldap_entry_attribute_') }.each do |dialog_ldap_entry_attribute, dialog_ldap_entry_attribute_value|
+                    log(:info, "LDAP entry attribute dialog element: { dialog_ldap_entry_attribute => #{dialog_ldap_entry_attribute}, dialog_ldap_entry_attribute_value => #{dialog_ldap_entry_attribute_value} }") if @DEBUG
+                    dialog_ldap_entry_attribute_name = dialog_ldap_entry_attribute.to_s.match(/dialog_ldap_entry_attribute_(.*)/i).captures[0]
+                    ldap_entry_attributes[dialog_ldap_entry_attribute_name] = dialog_ldap_entry_attribute_value.split("\n")
+                    log(:info, "Set LDAP entry attribute to value from dialog: { ldap_attribute => #{dialog_ldap_entry_attribute_name}, ldap_attribute_values => #{ldap_entry_attributes[dialog_ldap_entry_attribute_name]} }") if @DEBUG
+                  end
+                end
+
+                # IMPLEMENTERS: DO NOT MODIFY
+                #
+                # return munged LDAP entry attributes
+                @handle.object['ldap_entry_attributes'] = ldap_entry_attributes
+                log(:info, "$evm.object['ldap_entry_attributes'] => #{@handle.object['ldap_entry_attributes']})") if @DEBUG
+              end         
+            end
+
+          end
+        end
+      end
     end
   end
+end
 
-  # IMPLEMENTERS: DO NOT MODIFY
-  #
-  # return munged LDAP entry attributes
-  $evm.object['ldap_entry_attributes'] = ldap_entry_attributes
-  $evm.log(:info, "$evm.object['ldap_entry_attributes'] => #{$evm.object['ldap_entry_attributes']})") if @DEBUG
+if __FILE__ == $PROGRAM_NAME
+  RedHatConsulting_LDAP::Integration::LDAP::Operations::Methods::MungeLDAPEntryAttributes.new.main
 end
